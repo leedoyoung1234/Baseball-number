0bc375d8889f4592834e396a9d05ab1752769886
dylee <2do00531@naver.com>
2025-05-02 16:34:10 +0900

    baseball last

--- baseball/baseball_test.cpp
+++ baseball/baseball_test.cpp
@@ -1,8 +1,49 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballTest, Test1) {
-	EXPECT_EQ(1, 1);
+class BaseballFixture : public ::testing::Test {
+public:
+	Baseball game{ "123" };
+	void assertIllegalArgument(std::string quessNumber) {
+		try {
+			game.guess(quessNumber);
+			FAIL();
+		}
+		catch (std::exception& e) {
+			//PASS
+		}
+	}
+};
+
+TEST_F(BaseballFixture, Invalid_Case) {
+	assertIllegalArgument("12s");
+	assertIllegalArgument("12");
+	assertIllegalArgument("121");
+};
+
+
+TEST_F(BaseballFixture, S2B0) {
+	GuessResult result = game.guess("103");
+
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(result.strikes, 2);
+	EXPECT_EQ(result.balls, 0);
+};
+
+TEST_F(BaseballFixture, S1B2) {
+	GuessResult result = game.guess("132");
+
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(result.strikes, 1);
+	EXPECT_EQ(result.balls, 2);
+};
+
+TEST_F(BaseballFixture, MatchedNumberCase) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(result.strikes, 3);
+	EXPECT_EQ(result.balls, 0);
 };
 
 
